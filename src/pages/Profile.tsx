05cd28ef6aa6f7002d921fc668a0bb9ef6b92db4
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-04-27 14:05:52 +0300

    added user logout

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,41 @@
-import React, { FC, useState, useEffect } from 'react'
-import { getAuth, User } from 'firebase/auth'
+import React, { FC, useState } from 'react'
+import { getAuth } from 'firebase/auth'
 import { app } from '../firebase.config'
+import { useNavigate } from 'react-router-dom'
+
+interface IProfileFormData {
+  name: string | undefined | null
+  email: string | undefined | null
+}
 
 const Profile: FC = () => {
-  const [user, setUser] = useState<User | null>(null)
+  const auth = getAuth(app)
+  const [formData, setFormData] = useState<IProfileFormData>({
+    name: auth.currentUser?.displayName,
+    email: auth.currentUser?.email,
+  })
+  const { name, email } = formData
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const auth = getAuth(app)
-    setUser(auth.currentUser)
-  }, [])
+  const handleLogout = async () => {
+    try {
+      await auth.signOut()
+      navigate('/')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log(error.message)
+      }
+    }
+  }
 
-  return user ? (
-    <div>
-      <h1>{user.displayName}</h1>
-    </div>
-  ) : (
-    <div>
-      <h1>Not Logged In</h1>
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' type='button' onClick={handleLogout}>
+          Logout
+        </button>
+      </header>
     </div>
   )
 }
