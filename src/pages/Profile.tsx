aeb24c13cb2a05538e626cf4bcf7e1331ddb4b96
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-04-27 15:40:35 +0300

    added update user details in firestore

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,18 +1,22 @@
-import React, { FC, useState } from 'react'
-import { getAuth } from 'firebase/auth'
+import React, { ChangeEvent, FC, useState } from 'react'
+import { getAuth, updateProfile } from 'firebase/auth'
 import { app } from '../firebase.config'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from '../firebase.config'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 interface IProfileFormData {
-  name: string | undefined | null
-  email: string | undefined | null
+  name: string
+  email: string
 }
 
 const Profile: FC = () => {
   const auth = getAuth(app)
+  const [changeDetails, setChangeDetails] = useState<boolean>(false)
   const [formData, setFormData] = useState<IProfileFormData>({
-    name: auth.currentUser?.displayName,
-    email: auth.currentUser?.email,
+    name: auth.currentUser?.displayName || '',
+    email: auth.currentUser?.email || '',
   })
   const { name, email } = formData
   const navigate = useNavigate()
@@ -28,6 +32,40 @@ const Profile: FC = () => {
     }
   }
 
+  const handleSubmit = async () => {
+    try {
+      if (auth.currentUser && auth.currentUser?.displayName !== name) {
+        // Update display name in fb
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        // Update in firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error('Could not update profile details')
+        console.log(error.message)
+      }
+    }
+  }
+
+  const handleChangeDetails = () => {
+    changeDetails && handleSubmit()
+    setChangeDetails((prev) => !prev)
+  }
+
+  const handleChangeValues = (event: ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [event.target.id]: event.target.value,
+    }))
+  }
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
@@ -36,6 +74,36 @@ const Profile: FC = () => {
           Logout
         </button>
       </header>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p className='changePersonalDetails' onClick={handleChangeDetails}>
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={handleChangeValues}
+            ></input>
+            <input
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={handleChangeValues}
+            ></input>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
