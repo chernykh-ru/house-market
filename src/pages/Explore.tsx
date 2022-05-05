04b72e6ff496fbe0e8b36ff38314a5eb1d8fbe75
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-05 15:08:32 +0300

    added layout explore page

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,11 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
 
 const Explore: FC = () => {
   return (
-    <div>
-      <h1>Explore</h1>
-    </div>
-  );
-};
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
 
-export default Explore;
+      <main>
+        {/* TODO Slider */}
+        {/* <Slider /> */}
+
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to='/category/rent'>
+            <img
+              src={rentCategoryImage}
+              alt='rent'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for rent</p>
+          </Link>
+          <Link to='/category/sale'>
+            <img
+              src={sellCategoryImage}
+              alt='sell'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Explore
