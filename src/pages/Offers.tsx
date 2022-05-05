008a96b0942a94410abee63c0027c8463d6f55bf
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-05 21:08:10 +0300

    added offers layout

--- src/pages/Offers.tsx
+++ src/pages/Offers.tsx
@@ -1,11 +1,96 @@
-import React, { FC } from 'react';
+import React, { useState, useEffect } from 'react'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+  DocumentData,
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+import { IListing } from '../types/types'
+import ListingItem from '../components/ListingItem'
 
-const Offers: FC = () => {
+export interface IListings {
+  id: string
+  data: IListing
+}
+
+const Offers = () => {
+  const [listings, setListings] = useState<IListings[] | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query
+        const q = query(
+          listingsRef,
+          where('offer', '==', true),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        const listings: IListings[] = []
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data() as IListing,
+          })
+        })
+
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        if (error instanceof Error) {
+          toast.error('Could not fetch listings')
+        }
+      }
+    }
+    fetchListings()
+  }, [])
+
   return (
-    <div>
-      <h1>Offers</h1>
-    </div>
-  );
-};
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>Offers</p>
+      </header>
 
-export default Offers;
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+
+          <br />
+          <br />
+        </>
+      ) : (
+        <p>There are no current offers</p>
+      )}
+    </div>
+  )
+}
+
+export default Offers
