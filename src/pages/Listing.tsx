8bbb1223e1ec304de46dc30805e50581065a288a
chernykh-ru <86768486+chernykh-ru@users.noreply.github.com>
2022-05-08 17:00:57 +0300

    added listing details

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getAuth } from 'firebase/auth'
 import { doc, getDoc } from 'firebase/firestore'
-import { useNavigate, useParams } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { app, db } from '../firebase.config'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
@@ -38,7 +38,87 @@ const Listing = () => {
     return <Spinner />
   }
 
-  return <div>{listing && listing.name}</div>
+  if (!listing) {
+    return (
+      <div className='listingDetails'>
+        <p className='listingName'>Oops, something went wrong...</p>
+        <button className='primaryButton' onClick={() => navigate('/')}>
+          Back to Home
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      {/* SLIDER */}
+
+      <div
+        className='shareIconDiv'
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href)
+          setShareLinkCopied(true)
+          setTimeout(() => {
+            setShareLinkCopied(false)
+          }, 2000)
+        }}
+      >
+        <img src={shareIcon} alt='' />
+      </div>
+
+      {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>}
+
+      <div className='listingDetails'>
+        <p className='listingName'>
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className='listingLocation'>{listing.location}</p>
+        <p className='listingType'>
+          For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className='discountPrice'>
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className='listingDetailsList'>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : '1 Bedroom'}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : '1 Bathroom'}
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <p className='listingLocationTitle'>Location</p>
+
+        {/* MAP */}
+
+        {auth && auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className='primaryButton'
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
+  )
 }
 
 export default Listing
